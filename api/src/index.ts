98b18f5b36bdd92c42c2b8cb52335cf18b828d6f
Jason Katz <jason@nate.tech>
2021-11-08 17:42:50 -0500

    Add express boilerplate

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello, world');
+import express, { Response } from 'express';
+
+const app = express()
+const port = 3000
+
+app.get('/', (_, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`API listening at http://localhost:${port}`)
+})
