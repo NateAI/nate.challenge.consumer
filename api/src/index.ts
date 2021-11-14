b6f511c38ea9d2c0bf15c88d19cc1ca5f90f360b
Jason Katz <jason@nate.tech>
2021-11-14 12:38:04 -0500

    Add sample product data to API

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,32 @@
 import express, { Response } from 'express';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (_, res: Response) => {
-  res.send('Hello World!')
-})
+const products = [
+  {
+    id: 1,
+    productUrl: "google.com",
+    productPrice: "$7.99",
+    discountCode: "FREE5FROMNATE"
+  },
+  {
+    id: 2,
+    productUrl: "facebook.com",
+    productPrice: "$34.88"
+  },
+  {
+    id: 3,
+    productUrl: "apple.com",
+    productPrice: "$1000.00",
+    discountCode: "GIVEME50"
+  }
+];
+
+app.get('/products', (_, res: Response) => {
+  res.send(products)
+});
 
 app.listen(port, () => {
   console.log(`API listening at http://localhost:${port}`)
-})
+});
