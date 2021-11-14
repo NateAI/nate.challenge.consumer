18078a06d9ac9b04e707408966fff235c5ee4cf8
Jason Katz <jason@nate.tech>
2021-11-14 13:03:53 -0500

    Add product table to app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,33 @@
-import { Navbar, Container } from 'react-bootstrap';
+import { Container, Navbar, Table } from 'react-bootstrap';
 
 function App() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">nate</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      </Container>
-    </Navbar>
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">nate</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        </Container>
+      </Navbar>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>URL</th>
+            <th>Price</th>
+            <th>Discount Code</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>google.com</td>
+            <td>$7.99</td>
+            <td>FREE5FROMNATE</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
