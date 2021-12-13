ba063224c86856924e923338f956cfe1dfd4b1b3
Jason Katz <jason@nate.tech>
2021-12-13 14:07:41 -0500

    Add logged in user object

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -8,6 +8,13 @@ interface Product {
 const API_URL = 'http://localhost:8080';
 const PRODUCTS_ENDPOINT = '/products';
 
+const LOGGED_IN_USER = {
+  name: 'John Doe',
+  customerId: '123',
+  defaultPaymentMethodId: '456',
+  defaultAddressId: '789'
+};
+
 function App() {
   return (
     <h1>Hello, world!</h1>
