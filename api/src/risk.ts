c67870dd4f1efe101377a5eb04f8a3c098084de7
joe olley <joeolley@hotmail.co.uk>
2022-03-10 08:45:55 +0000

    challenge updates

--- api/src/risk.ts
+++ api/src/risk.ts
@@ -1,3 +1,4 @@
+// note - the signature of this function can be modified to suit any assumptions made about the risk flow
 const assessRisk = async (paymentMethodId: string): Promise<boolean> => {
   return true;
 };
