d6e81361a55d296c4fad6805b493ef90280a63cb
Jason Katz <jason@nate.tech>
2022-04-21 11:49:19 -0400

    Fix field name

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,18 +7,18 @@ const port = 8080;
 
 const products = [
   {
-    id: 1,
+    productId: 1,
     productUrl: "https://www.bedbathandbeyond.com/store/product/kate-spade-new-york-2-slice-toaster/5374432",
     productPrice: 9499,
     discountCode: "FREE5FROMNATE"
   },
   {
-    id: 2,
+    productId: 2,
     productUrl: "https://www.oculus.com/quest-2/",
     productPrice: 29900
   },
   {
-    id: 3,
+    productId: 3,
     productUrl: "https://www.apple.com/shop/product/MME73AM/A/airpods-3rd-generation",
     productPrice: 17900,
     discountCode: "GIVEME50"
